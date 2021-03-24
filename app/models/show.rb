604e3c2d968fe88d790c53231cbf2a6bcb82665f
Hatice <haticekaraty@gmail.com>
2021-03-24 15:16:17 -0400

    commit tree 1f741d3ab45902a08b23db2d1cebd9919c949866

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
 class Show < ActiveRecord::Base
-  
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+    
+    
+    def actors_list
+       # "#{self.actors.first.first_name} #{self.actors.first.last_name}"
+        
+        self.actors.collect do |actor|
+            "#{actor.first_name} #{actor.last_name}"
+        end
+        #binding.pry
+    end
 end
